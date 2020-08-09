d82f9e89177cf8bc8bb99b6be7f06fc6b7d998a2
Joseph Zurowski <Dunklheit@users.noreply.github.com>
2020-08-09 15:27:13 -0400

    Add SDK Init Sanity Check

--- src/lib.rs
+++ src/lib.rs
@@ -3,3 +3,48 @@
 #![allow(non_snake_case)]
 
 include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
+
+#[cfg(test)]
+mod tests {
+  use super::*;
+  use std::ffi::CString;
+  use std::mem;
+
+  #[test]
+  fn sdk_initialization() {
+    unsafe {
+      let mut default_config: vx_sdk_config_t = mem::zeroed();
+      let mut status = vx_get_default_config3(
+        &mut default_config,
+        mem::size_of::<vx_sdk_config_t>() as size_t
+      );
+
+      if status != VX_E_SUCCESS as i32 {
+        let err_str = vx_get_error_string(status) as *mut i8;
+
+        println!("vx_get_default_config3() returned {}: {}",
+          status,
+          CString::from_raw(
+            err_str
+          ).into_string().expect("Unable to get error string!"));
+      }
+
+      status = vx_initialize3(
+        &mut default_config,
+        mem::size_of::<vx_sdk_config_t>() as size_t
+      );
+
+      if status != VX_E_SUCCESS as i32 {
+        let err_str = vx_get_error_string(status) as *mut i8;
+
+        println!("vx_initialize3() returned {}: {}",
+          status,
+          CString::from_raw(
+            err_str
+          ).into_string().expect("Unable to get error string!"));
+      }
+
+      assert_eq!(status, (VX_E_SUCCESS as i32));
+    }
+  }
+}
