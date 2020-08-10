0c7aaa0e6801f452423488e6d6bb1886d6156741
Joseph Zurowski <Dunklheit@users.noreply.github.com>
2020-08-10 13:37:41 -0400

    Connect to Echo channel

--- src/lib.rs
+++ src/lib.rs
@@ -12,6 +12,7 @@ use serde::{Serialize, Deserialize};
 include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
 
 static mut connected: bool = false;
+static mut logged_in: bool = false;
 
 pub fn hello_vivox() {
   use std::{thread, time::Duration};
@@ -41,8 +42,20 @@ pub fn hello_vivox() {
     }
   }
 
-  println!("Logging in...");
   login();
+
+  println!("Waiting until login completes...");
+
+  unsafe {
+    loop {
+      if logged_in {
+        break;
+      }
+    }
+  }
+
+  println!("Joining echo channel...");
+  join_echo();
 }
 
 fn init() -> i32 {
@@ -116,7 +129,7 @@ fn login() {
     (*req).access_token = vx_strdup(
       CString::new(
         vx_generate_token(
-          "get_your_own_key!",
+          "get_your_own!",
           "gmclvivox-gmvivox-w-dev",
           SystemTime::now()
             .duration_since(SystemTime::UNIX_EPOCH)
@@ -126,6 +139,65 @@ fn login() {
           1,
           "sip:.gmclvivox-gmvivox-w-dev.dunkel.@vdx5.vivox.com",
           None,
+        )
+      )
+        .expect("Unable to allocate string")
+        .as_ptr()
+    );
+
+    vx_issue_request(&mut (*req).base);
+  }
+}
+
+fn join_echo() {
+  use std::ffi::CString;
+  use std::mem;
+  use std::time::SystemTime;
+
+  unsafe {
+    let mut req: *mut vx_req_sessiongroup_add_session = mem::zeroed();
+    vx_req_sessiongroup_add_session_create(&mut req);
+
+    (*req).sessiongroup_handle = vx_strdup(
+      CString::new("sg1")
+        .expect("Unable to allocate string")
+        .as_ptr()
+    );
+
+    (*req).session_handle = vx_strdup(
+      CString::new("echotest")
+        .expect("Unable to allocate string")
+        .as_ptr()
+    );
+
+    (*req).uri = vx_strdup(
+      CString::new("sip:confctl-e-gmclvivox-gmvivox-w-dev.echotest@vdx5.vivox.com")
+        .expect("Unable to allocate string")
+        .as_ptr()
+    );
+
+    (*req).connect_audio = 1;
+    (*req).connect_text = 1;
+
+    (*req).account_handle = vx_strdup(
+      CString::new(".gmclvivox-gmvivox-w-dev.dunkel.")
+        .expect("Unable to allocate string")
+        .as_ptr()
+    );
+
+    (*req).access_token = vx_strdup(
+      CString::new(
+        vx_generate_token(
+          "get_your_own!",
+          "gmclvivox-gmvivox-w-dev",
+          SystemTime::now()
+            .duration_since(SystemTime::UNIX_EPOCH)
+            .expect("Back from the future")
+            .as_secs() + 240,
+          "join",
+          8,
+          "sip:.gmclvivox-gmvivox-w-dev.dunkel.@vdx5.vivox.com",
+          Some("sip:confctl-e-gmclvivox-gmvivox-w-dev.echotest@vdx5.vivox.com".to_string()),
         )
       )
         .expect("Unable to allocate string")
@@ -146,7 +218,6 @@ fn poll_loop() {
       let status = vx_get_message(&mut m);
 
       if status == VX_GET_MESSAGE_AVAILABLE as i32 {
-        println!("MSG AVAILABLE!");
         message_handler(m);
         vx_destroy_message(m);
         // Handle to `m` becomes invalid here
@@ -185,7 +256,6 @@ fn message_handler(msg: *mut vx_message_base_t) {
 
 fn response_handler(resp: *mut vx_resp_base_t) {
   use std::ffi::CStr;
-  println!("RESP");
 
   unsafe {
     if (*resp).return_code == 1 {
@@ -204,22 +274,58 @@ fn response_handler(resp: *mut vx_resp_base_t) {
   }
 }
 
+#[allow(unused_variables)]
 fn event_handler(evt: *mut vx_evt_base_t) {
+  use std::ffi::CStr;
   use std::mem::transmute;
-
-  println!("EVT");
 
   unsafe {
     match (*evt).type_ {
-      evt_account_login_state_change => {
+      vx_event_type_evt_account_login_state_change => {
         println!("[Vivox] Login state changed");
   
         let act = transmute::<*mut vx_evt_base_t, *mut vx_evt_account_login_state_change>(&mut *evt);
 
         if (*act).state == vx_login_state_change_state_login_state_logged_in {
-          println!("I have logged in!");
+          logged_in = true;
         }
-      }
+      },
+      vx_event_type_evt_media_stream_updated => {
+        let session = transmute::<*mut vx_evt_base_t, *mut vx_evt_media_stream_updated>(&mut *evt);
+
+        match (*session).state {
+          vx_session_media_state_session_media_connected => println!(
+            "Connected to voice channel: {}",
+            CStr::from_ptr((*session).session_handle)
+              .to_str()
+              .expect("Not a valid string")
+          ),
+          vx_session_media_state_session_media_disconnected => {
+            match (*session).status_code {
+              0 => println!(
+                "Disconnected from voice channel: {}",
+                CStr::from_ptr((*session).session_handle)
+                  .to_str()
+                  .expect("Not a valid string")
+              ),
+              val => {
+                println!(
+                  "Disconnected from voice channel: {}, error {}: {}",
+                  CStr::from_ptr((*session).session_handle)
+                    .to_str()
+                    .expect("Not a valid string"),
+                  (*session).status_code,
+                  CStr::from_ptr(vx_get_error_string((*session).status_code))
+                    .to_str()
+                    .expect("Not a valid string")
+                );
+              }
+            }
+          },
+          _ => {},
+        }
+      },
+      _ => {},
     }
   }
 }
@@ -255,8 +361,9 @@ struct TokenRequest {
   exp: u64,
   vxa: String,
   vxi: u32,
-  t: Option<String>,
   f: String,
+  #[serde(skip_serializing_if = "Option::is_none")]
+  t: Option<String>,
 }
 
 fn vx_generate_token(
@@ -280,8 +387,8 @@ fn vx_generate_token(
     exp,
     vxa: vxa.to_string(),
     vxi,
-    t,
     f: f.to_string(),
+    t,
   };
   let mut obj = serde_json::to_string(&tr)
     .expect("Unable to serialize object!");
@@ -305,8 +412,6 @@ fn vx_generate_token(
   let signed_payload = BASE64URL_NOPAD.encode(&res.into_bytes());
 
   // Combine header and payload with signature
-  println!("{}.{}", to_sign, signed_payload);
-
   format!("{}.{}", to_sign, signed_payload)
 }
 
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,4 @@
 use std::io::{self, Read};
-use std::thread;
 
 use vivoxrs::hello_vivox;
 
