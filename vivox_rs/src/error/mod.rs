add83ad512403b54546342bc131602d29033dff4
Joseph Zurowski <Dunklheit@users.noreply.github.com>
2020-08-13 21:39:40 -0400

    Impl bug fixes

--- vivox_rs/src/error/mod.rs
+++ vivox_rs/src/error/mod.rs
@@ -128,17 +128,18 @@ impl fmt::Debug for VivoxError {
   fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
     f.debug_struct("VivoxError")
       .field("error_code", &(*self as i32))
-      .field("explanation", format!("{}", self))
+      .field("explanation", &format!("{}", self))
       .finish()
   }
 }
 
 impl fmt::Display for VivoxError {
   fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-    let err = vx_get_error_string(*self as i32) as *mut i8;
+    let self_int = *self as i32;
+    let err = unsafe { vx_get_error_string(self_int) as *mut i8 };
 
     let conv_str = unsafe { CStr::from_ptr(err).to_str().unwrap() };
 
-    write!(f, "{}", conv_str)
+    write!(f, "Code {}: '{}'", self_int, conv_str)
   }
 }
